ea48e9a832f3adc46013fb483d3c305438cf356a
David Zulaica <zulaica@gmail.com>
2018-01-12 16:51:43 -0800

    🆕 🛠  onEventMethod

--- source/utilities/event-listener.ts
+++ source/utilities/event-listener.ts
@@ -5,3 +5,9 @@ interface OnEventMethodInterface {
     options?: boolean | undefined
   ) => void;
 }
+
+const onEventMethod = (
+  event: string,
+  eventListener: EventListenerOrEventListenerObject,
+  options = false
+) => addEventListener(event, eventListener, options);
