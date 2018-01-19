8e5e4a752d0a5041838ad49fefd0d0934f5e02ce
David Zulaica <zulaica@gmail.com>
2018-01-18 16:06:12 -0800

    🔄 🛠  ES module import workaround

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,12 @@
-import './utilities/event-listener';
+/*
+ * Pure ES Module support in browsers requires either a file extension or a js
+ * MIMETYPE to resolve correctly. As a workaround, TSC will resolve an import
+ * with a non `.ts` file extension allowing the code to compile with the
+ * correct reference.
+ *
+ * https://github.com/Microsoft/TypeScript/issues/16577#issuecomment-309169829
+ */
+import './utilities/event-listener.js';
 
 window.on('load', () =>
   document.body.appendChild(document.createTextNode(`Hello, y’all!`))
