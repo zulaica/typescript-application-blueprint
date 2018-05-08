c30ff821495ec70c0a9124383d429c01350143c9
David Zulaica <zulaica@gmail.com>
2018-05-07 18:15:49 -0700

    🔄 🛠  Pad single digit months

--- devserver.js
+++ devserver.js
@@ -9,7 +9,7 @@ const server = http.createServer((request, response) => {
 function timestamp() {
   const date = new Date();
   const year = date.getFullYear().toString();
-  const month = (date.getMonth() + 1).toString();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const day = date
     .getDate()
     .toString()
