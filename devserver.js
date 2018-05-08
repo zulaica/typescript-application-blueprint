170ce3bda440a84e93e4cded23b63f7f533eb4d6
David Zulaica <zulaica@gmail.com>
2018-05-07 18:41:08 -0700

    🔄 🛠  Clear console when starting devserver

--- devserver.js
+++ devserver.js
@@ -22,5 +22,6 @@ const timestamp = () => {
 };
 
 server.listen(port);
+console.log('\x1Bc');
 console.log(`${timestamp()} Starting up development server.`);
 console.log('Type CTRL-C to stop the development server.');
