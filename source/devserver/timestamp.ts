17a080e2cc1ca9e67f796f71c56f188b621faa75
David Zulaica <zulaica@gmail.com>
2018-07-06 15:56:56 -0700

    🆕 🛠  Move timestamp into new ts file for devserver

---
+++ source/devserver/timestamp.ts
@@ -0,0 +1,23 @@
+const timestamp = () => {
+  const date = new Date();
+  const year = date.getFullYear().toString();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date
+    .getDate()
+    .toString()
+    .padStart(2, '0');
+  const hour = date
+    .getHours()
+    .toString()
+    .padStart(2, '0');
+  const minute = date
+    .getMinutes()
+    .toString()
+    .padStart(2, '0');
+  const second = date
+    .getSeconds()
+    .toString()
+    .padStart(2, '0');
+
+  return `[${year}-${month}-${day} ${hour}:${minute}:${second}]`;
+};
