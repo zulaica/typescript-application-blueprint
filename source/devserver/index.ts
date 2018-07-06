b0b0fd5657e7f107e822c38cf78d48bcb5fc3980
David Zulaica <zulaica@gmail.com>
2018-07-06 15:50:24 -0700

    🔄 🛠  Refactor newline generation in server logging

--- source/devserver/index.ts
+++ source/devserver/index.ts
@@ -6,12 +6,10 @@ server.listen(config.port, config.ipAddress, () => {
   const serverAddress = server.address() as AddressInfo;
 
   process.stdout.write('\x1Bc');
-  process.stdout.write('Starting up development server.');
-  process.stdout.write('\n');
+  process.stdout.write('Starting up development server.\n');
   process.stdout.write(
-    `Serving on http://${serverAddress.address}:${serverAddress.port}/`
+    `Serving on http://${serverAddress.address}:${serverAddress.port}/\n`
   );
-  process.stdout.write('\n');
-  process.stdout.write('Type CTRL-C to stop the development server.');
+  process.stdout.write('Type CTRL-C to stop the development server.\n');
   process.stdout.write('\n');
 });
