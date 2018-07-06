2dca9c9c36cc33c3b952e8b05b24af9b7b33d7b2
David Zulaica <zulaica@gmail.com>
2018-07-06 00:37:14 -0700

    🆕 🛠  Manually add newlines normally supplied by console.log

--- source/devserver/devserver.ts
+++ source/devserver/devserver.ts
@@ -33,8 +33,11 @@ server.listen(config.port, config.ipAddress, () => {
 
   process.stdout.write('\x1Bc');
   process.stdout.write('Starting up development server.');
+  process.stdout.write('\n');
   process.stdout.write(
     `Serving on http://${serverAddress.address}:${serverAddress.port}/`
   );
+  process.stdout.write('\n');
   process.stdout.write('Type CTRL-C to stop the development server.');
+  process.stdout.write('\n');
 });
