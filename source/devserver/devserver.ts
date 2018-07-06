3f1e585707193bf52d44852d137db893b02da479
David Zulaica <zulaica@gmail.com>
2018-07-05 23:11:35 -0700

    🔄 🛠  Refactor filePath, introduce fullPath

--- source/devserver/devserver.ts
+++ source/devserver/devserver.ts
@@ -20,18 +20,19 @@ const MIMETYPES: MimeTypesInterface = Object.freeze({
 
 const server = http.createServer((request, response) => {
   const relativeUri = url.parse(request.url as string).pathname as string;
-  let filepath: string = path.join(process.cwd(), relativeUri);
-  if (fs.existsSync(filepath)) {
-    fs.statSync(filepath).isDirectory() ? (filepath += 'index.html') : filepath;
-  }
+  const filePath: string = path.join(process.cwd(), relativeUri);
+  const fullPath: string =
+    fs.existsSync(filePath) && fs.statSync(filePath).isDirectory()
+      ? filePath + 'index.html'
+      : filePath;
 
-  fs.readFile(filepath, (error, data) => {
+  fs.readFile(fullPath, (error, data) => {
     if (error) {
       response.writeHead(404, { 'Content-Type': 'text/plain' });
       response.write('404 Not Found');
       response.end();
     } else {
-      const contentType = MIMETYPES[path.extname(filepath)];
+      const contentType = MIMETYPES[path.extname(fullPath)];
       response.writeHead(200, { 'Content-Type': contentType });
       response.write(data);
       response.end();
