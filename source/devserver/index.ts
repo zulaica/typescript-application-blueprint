1e625efa825d4825d237cb8c714a01cd823f0d03
David Zulaica <zulaica@gmail.com>
2018-07-06 12:15:23 -0700

    🔄 🛠  Revert destructuring

--- source/devserver/index.ts
+++ source/devserver/index.ts
@@ -2,10 +2,8 @@ import { AddressInfo } from 'net';
 import * as config from './config';
 import { server } from './server';
 
-const { listen, address } = server;
-
-listen(config.port, config.ipAddress, () => {
-  const serverAddress = address() as AddressInfo;
+server.listen(config.port, config.ipAddress, () => {
+  const serverAddress = server.address() as AddressInfo;
 
   process.stdout.write('\x1Bc');
   process.stdout.write('Starting up development server.');
