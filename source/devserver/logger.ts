4b5e333c2147ba833a032ae35c91534c7e224e12
David Zulaica <zulaica@gmail.com>
2018-07-06 17:18:04 -0700

    🆕 🛠  Follow example in NodeJS documentation

--- source/devserver/logger.ts
+++ source/devserver/logger.ts
@@ -1,7 +1,11 @@
 import { Console } from 'console';
+import * as fs from 'fs';
 import { timestamp } from './timestamp';
 
-export const logger = new Console();
+const output = fs.createWriteStream('./stdout.log');
+const errorOutput = fs.createWriteStream('./stderr.log');
+
+export const logger = new Console(output, errorOutput);
 
 logger.info = message => process.stdout.write(`${message}\n`);
 logger.log = message => process.stdout.write(`${timestamp()} ${message}\n`);
