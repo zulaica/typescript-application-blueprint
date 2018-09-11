043a4463b83aa0a267e569cd91eb10ce4e815296
David Zulaica <zulaica@gmail.com>
2018-09-10 17:12:50 -0700

    🔄 🛠  Rename variables for clarity

--- source/devserver/server.ts
+++ source/devserver/server.ts
@@ -6,25 +6,26 @@ import { logger } from './logger';
 import { MIMETYPES } from './mimetypes';
 
 export const server = http.createServer((request, response) => {
-  const relativeUri = url.parse(request.url as string).pathname as string;
-  const filePath: string = path.join(process.cwd(), relativeUri);
-  const fullPath: string =
-    fs.existsSync(filePath) && fs.statSync(filePath).isDirectory()
-      ? filePath + 'index.html'
-      : filePath;
+  const relativeFilePath = url.parse(request.url as string).pathname as string;
+  const absoluteFilePath: string = path.join(process.cwd(), relativeFilePath);
+  const resolvedFilePath: string =
+    fs.existsSync(absoluteFilePath) &&
+    fs.statSync(absoluteFilePath).isDirectory()
+      ? absoluteFilePath + 'index.html'
+      : absoluteFilePath;
 
-  fs.readFile(fullPath, (error, data) => {
+  fs.readFile(resolvedFilePath, (error, data) => {
     if (error) {
       response.writeHead(404, { 'Content-Type': 'text/plain' });
       response.write('404 Not Found');
       response.end();
-      logger.error(`${response.statusCode} | ${relativeUri}`);
+      logger.error(`${response.statusCode} | ${relativeFilePath}`);
     } else {
-      const contentType = MIMETYPES[path.extname(fullPath)];
+      const contentType = MIMETYPES[path.extname(resolvedFilePath)];
       response.writeHead(200, { 'Content-Type': contentType });
       response.write(data);
       response.end();
-      logger.log(`${response.statusCode} | ${relativeUri}`);
+      logger.log(`${response.statusCode} | ${relativeFilePath}`);
     }
   });
 });
