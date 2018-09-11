2d8db0b4e765dd7810f82c0627664d9d69f41c56
David Zulaica <zulaica@gmail.com>
2018-09-11 00:29:42 -0700

    🔄 🛠   Refactor code with handleUnsupported function

--- source/index.ts
+++ source/index.ts
@@ -12,17 +12,19 @@ import './utilities/feature-detection.js';
 const handleSuccess = () =>
   document.body.appendChild(document.createTextNode(document.title));
 
+const handleUnsupported = (unsupportedFeatures: string[]) => {
+  const list = document.body.appendChild(document.createElement('dl'));
+  const term = document.createElement('dt');
+  list.appendChild(term);
+  term.appendChild(document.createTextNode('Unsupported Features'));
+  for (const feature of unsupportedFeatures) {
+    const description = list.appendChild(document.createElement('dd'));
+    description.appendChild(document.createTextNode(`${feature}`));
+  }
+};
+
 window.on('load', () =>
   navigator
     .supportFor(['requestMIDIAccess', 'DeviceMotionEvent'])
-    .then(handleSuccess, unsupportedFeatures => {
-      const list = document.body.appendChild(document.createElement('dl'));
-      const term = document.createElement('dt');
-      list.appendChild(term);
-      term.appendChild(document.createTextNode('Unsupported Features'));
-      for (const feature of unsupportedFeatures) {
-        const description = list.appendChild(document.createElement('dd'));
-        description.appendChild(document.createTextNode(`${feature}`));
-      }
-    })
+    .then(handleSuccess, handleUnsupported)
 );
