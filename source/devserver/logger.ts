58bc94a70b83415cbebec22cfba7441e289743dc
David Zulaica <zulaica@gmail.com>
2018-12-17 22:56:58 -0800

    🆕 🛠  Format logger messages with color

--- source/devserver/logger.ts
+++ source/devserver/logger.ts
@@ -1,4 +1,5 @@
 import { Console } from 'console';
+import * as util from 'util';
 import { timestamp } from './timestamp';
 
 const output = process.stdout;
@@ -6,6 +7,13 @@ const errorOutput = process.stderr;
 
 export const logger = new Console(output, errorOutput);
 
-logger.error = message => process.stderr.write(`${timestamp()} ${message}\n`);
-logger.info = message => process.stdout.write(`${message}\n`);
-logger.log = message => process.stdout.write(`${timestamp()} ${message}\n`);
+logger.error = message =>
+  process.stderr.write(
+    util.format('\x1b[91m%s\x1b[0m', `${timestamp()} ${message}\n`)
+  );
+logger.info = message =>
+  process.stdout.write(util.format('\x1b[34m%s\x1b[0m', `${message}\n`));
+logger.log = message =>
+  process.stdout.write(
+    util.format('\x1b[92m%s\x1b[0m', `${timestamp()} ${message}\n`)
+  );
